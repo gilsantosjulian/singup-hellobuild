405955bd4fd2e455aa09353c9319953b579c9872
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-04-06 23:27:30 -0500

    feat: add colors tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      primary: '#33C1BB',
+      teal: colors.teal,
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+      white: colors.white,
+    }
   },
   variants: {
     extend: {},
